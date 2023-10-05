6ad634326cbad7720cc79a3024044e4d1480f072
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-10-05 07:29:00 +0100

    Implement: Cards Color

--- src/routes/Tab3.svelte
+++ src/routes/Tab3.svelte
@@ -1,34 +1,109 @@
+<script>
+	import { onMount } from "svelte";
 
-<div class="card card-side glass">
+  const projects = [
+    { 
+      name:'Studio Umami',
+      desc:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quos commodi non nulla, dolore consequatur facere tempora atque error dolor est sequi odit eveniet, voluptatem quisquam porro vero culpa voluptas..',
+      alt: 'icon',
+      tech:['Svelte', 'JavaScript', 'TailwindCSS', 'Vercel'],
+      image:"/studio-umami.png",
+      liveUrl:'/',
+      sourceCode:'/',
+      color: 'rgba(255, 255, 255, 0.1)',
+    },
+    { 
+      name:'Landlrd',
+      desc:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quos commodi non nulla, dolore consequatur facere tempora atque error dolor est sequi odit eveniet, voluptatem quisquam porro vero culpa voluptas..',
+      alt: 'icon',
+      tech:['Ruby on Rails', 'JavaScript', 'Bootstrap', 'Render', 'PostgreSQL'],
+      image:"/studio-umami.png",
+      liveUrl:'/',
+      sourceCode:'/',
+      color: 'rgba(0, 128, 0, 0.1)',
+    },
+    { 
+      name:'GetLinked',
+      desc:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quos commodi non nulla, dolore consequatur facere tempora atque error dolor est sequi odit eveniet, voluptatem quisquam porro vero culpa voluptas..',
+      alt: 'icon',
+      tech:['Svelte', 'JavaScript', 'TailwindCSS', 'Vercel'],
+      image:"/studio-umami.png",
+      liveUrl:'/',
+      sourceCode:'/',
+      color: 'rgba(128, 0, 128, 0.1)',
+    },
+    { 
+      name:'Studio Umami',
+      desc:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quos commodi non nulla, dolore consequatur facere tempora atque error dolor est sequi odit eveniet, voluptatem quisquam porro vero culpa voluptas..',
+      alt: 'icon',
+      tech:['Svelte', 'JavaScript', 'TailwindCSS', 'Vercel'],
+      image:"/studio-umami.png",
+      liveUrl:'/',
+      sourceCode:'/',
+      color: 'rgba(255, 255, 255, 0.1)',
+    },
+  ];
+
+  onMount(()=>{
+      const cards = document.querySelectorAll('.color');
+    console.log(cards);
+    cards.forEach((card) => {
+      card.addEventListener('mouseenter', () => {
+        const index = card.getAttribute('data-index');
+        const hoverColor = projects[index].color;
+        card.style.setProperty('--hover-color', hoverColor);
+      });
+
+      card.addEventListener('mouseleave', () => {
+        card.style.removeProperty('--hover-color');
+      });
+    });
+  })
+  
+
+
+</script>
+
+{#each projects as project, i}
+<div class="card card-side glass color" key={i} data-index={i}>
   <figure>
-    <img src="/studio-umami.png" alt="car!" class="hover-image" />
+    <img src={project.image} alt class="hover-image" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">Studio Umami</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus quos commodi non nulla, dolore consequatur facere tempora atque error dolor est sequi odit eveniet, voluptatem quisquam porro vero culpa voluptas..</p>
+    <h2 class="card-title">{project.name}</h2>
+    <p>{project.desc}</p>
     <div class="badges">
-      <span class="badge badge-outline">Svelte</span>
-      <span class="badge badge-outline">JavaScript</span>
-      <span class="badge badge-outline">TailWindCSS</span>
-      <span class="badge badge-outline">Badge</span>
+      {#each project.tech as technology}
+      <span class="badge badge-outline">{technology}</span>
+      {/each}
     </div>
     <div class="card-actions justify-stretch">
-      <button class="btn btn-sm btn-primary">Live Link</button>
-      <button class="btn btn-sm btn-primary">Source Code</button>
+      <a href={project.liveUrl} class="btn btn-sm btn-primary">Live Link</a>
+      <a href={project.sourceCode} class="btn btn-sm btn-primary">Source Code</a>
     </div>
   </div>
 </div>
-
-
-
+{/each}
 
 <style>
+
+.color {
+  transition: background-color 0.3s ease; /* Add a transition effect for smoother color change */
+}
+
+.color:hover {
+  background-color: var(--hover-color); /* Use a CSS variable for the background color on hover */
+}
+
   .badges {
+    display: flex;
+    flex-wrap: wrap;
+    gap: .4em;
     margin-bottom: 1em;
 
   }
   span {
-    font-size: .5em;
+    font-size: .7em;  
   }
 
   h2 {
@@ -46,6 +121,7 @@
   .card {
   position: relative;
   flex-direction: column;
+  margin-bottom: 3em;
 }
 
 
